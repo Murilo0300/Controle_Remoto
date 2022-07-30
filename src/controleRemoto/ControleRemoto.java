4ffebabb288144173f7096cb4c6352f6c6036d3f
Murilo Cavalcanti <murilo0300@hotmail.com.br>
2022-07-30 20:52:10 -0300

    implementação da interface

--- src/controleRemoto/ControleRemoto.java
+++ src/controleRemoto/ControleRemoto.java
@@ -1,6 +1,41 @@
 package controleRemoto;
 
 public class ControleRemoto {
-	public int volume;
+	public int volume; 
+	public boolean ligado;
+	public boolean tocando;
+	
+	//metodos especias
+	public void controleRemoto() {
+		volume = 50;
+		ligado = false;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public boolean getLigado() {
+		return ligado;
+	}
+
+	public void setLigado(boolean ligado) {
+		this.ligado = ligado;
+	}
+
+	public boolean getTocando() {
+		return tocando;
+	}
+
+	public void setTocando(boolean tocando) {
+		this.tocando = tocando;
+	}
+	
+	
+	
 
 }
